14ae2193af34706230bc14f1a6705980cd82b4ca
Yusuf Eka <yusufblegoh@gmail.com>
2015-11-05 15:47:09 +0700

    nambah Controller

---
+++ app/Models/Tagihan.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blegoh
+ * Date: 05/11/15
+ * Time: 11:27
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tagihan extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'Tagihan';
+}
