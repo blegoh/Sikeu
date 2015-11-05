753a5676d434e62607776ec78552bb40f8da7d82
Yusuf Eka <yusufblegoh@gmail.com>
2015-11-05 11:03:39 +0700

    DB change

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => 'yusufeka.me',
-            'database'  => 'sikeu',
-            'username'  => 'sikeu',
-            'password'  => 'sikeu',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
