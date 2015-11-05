df25158c44d9737dc176104ab560d15a8a7f2227
Yusuf Eka <yusufblegoh@gmail.com>
2015-11-05 10:32:43 +0700

    First Commit

---
+++ app/Http/Controllers/TagihanController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blegoh
+ * Date: 05/11/15
+ * Time: 9:51
+ */
+
+namespace app\Http\Controllers;
+
+
+class TagihanController extends Controller
+{
+    public function show(){
+        return view('ukt.show');
+    }
+}
