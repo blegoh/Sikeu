1ebae0947733d490b4b80b614dec411640d0e265
Yusuf Eka <yusufblegoh@gmail.com>
2015-12-13 23:21:03 +0700

    nambah Controller

--- app/Http/Controllers/TagihanController.php
+++ app/Http/Controllers/TagihanController.php
@@ -6,12 +6,103 @@
  * Time: 9:51
  */
 
-namespace app\Http\Controllers;
+namespace App\Http\Controllers;
 
+use App\Models\Fakultas;
+use App\Models\Mahasiswa;
+use App\Models\Tagihan;
+use App\Models\TagihanPendaftar;
+use App\Models\TagihanUKT;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
 
 class TagihanController extends Controller
 {
-    public function show(){
-        return view('ukt.show');
+    private $user;
+
+    public function __construct()
+    {
+        $this->user = Auth::user();
     }
+
+    public function show($fakultasID = 0,$thnAkademik = 0){
+        $tagihans = new TagihanUKT();
+        $thnExist = DB::table('Tagihan')->select('ThnAkademik')->distinct()->get();
+        if($thnAkademik != 0){
+            $tagihans->setTahunAkademik($thnAkademik);
+        }
+        if($fakultasID != 0){
+            $tagihans->setFakultas($fakultasID);
+        }
+        $fakultas = Fakultas::all();
+
+        return view('tagihan.ukt',[
+            'tagihans' => $tagihans->getTagihan(),
+            'fakultas' => $fakultas,
+            'pilihan' => $fakultasID,
+            'thnExist' => $thnExist,
+            'thn' => $thnAkademik,
+            'user' => $this->user,
+            'belum' => $tagihans->getBelum(),
+            'sudah' => $tagihans->getSudah()
+        ]);
+    }
+
+    public function filter(Request $request){
+        $fakultas = $request->input('fakultas');
+        $thnAkademik = $request->input('thn');
+        return redirect('tagihan/ukt/'.$fakultas.'/'.$thnAkademik);
+    }
+
+
+    public function showWisuda(){
+        $tgl = Carbon::today();
+        $smt = ($tgl->month > 6)? 1 : 2;
+        $thn = ($smt == 1)? $tgl->year.$tgl->year+1 : ($tgl->year-1).$tgl->year;
+        $tagihans = Tagihan::where('SmtAkademik',$smt)
+            ->where('ThnAkademik',$thn)
+            ->where('JenisTagihanID','2')
+            ->get();
+
+        return view('tagihan.wisuda',[
+            'tagihans' => $tagihans
+        ]);
+    }
+
+    public function addTagihanUKT(){
+        DB::table('Mahasiswa')
+            ->update(['Aktif' => 0]);
+        //looping semua mahasiswa
+        $mhs = Mahasiswa::all();
+        $tgl = Carbon::today();
+        $smt = ($tgl->month > 6)? 1 : 2;
+        $thn = ($smt == 1)? $tgl->year.$tgl->year+1 : ($tgl->year-1).$tgl->year;
+        foreach ($mhs as $mhs) {
+            $tagihan = new Tagihan();
+            $tagihan->NIM = $mhs->NIM;
+            $tagihan->JenisTagihanID = 1;
+            $tagihan->ThnAkademik = $thn;
+            $tagihan->SmtAkademik = $smt;
+            $tagihan->Nominal = $mhs->ukt->Nominal;
+            $tagihan->save();
+        }
+        return redirect('tagihan/ukt');
+    }
+
+    public function showUM(){
+        $tgl = Carbon::today();
+        $thn = $tgl->year.$tgl->year+1;
+        $tagihans = TagihanPendaftar::where('ThnAkademik',$thn)
+            ->get();
+        return view('tagihan.um',[
+            'tagihans' => $tagihans
+        ]);
+    }
+
+    private function test(){
+
+    }
+
 }
--- app/Http/Controllers/UKTController.php
+++ app/Http/Controllers/UKTController.php
@@ -8,65 +8,167 @@
 
 namespace App\Http\Controllers;
 
-
+use DB;
+use App\Models\Fakultas;
 use App\Models\Prodi;
 use App\Models\Jalur;
+use App\Models\DanaUKT;
 use App\Models\UKT;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 class UKTController extends Controller
 {
+    private $user;
+
+    public function __construct()
+    {
+        $this->user = Auth::user();
+    }
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(){
-        $prodi = Prodi::all();
-        $jalur = Jalur::all();
-        $tgl = Carbon::today();
-        $thnAkademik = ($tgl->month > 6)? $tgl->year.$tgl->year+1 : ($tgl->year-1).$tgl->year;
+        $fakultas = Fakultas::all();
         return view('ukt.show',[
-            'prodis' => $prodi,
-            'jalurs' => $jalur,
-            'tgl' => $tgl,
-            'thnAkademik' => $thnAkademik
+            'fakultas' => $fakultas
         ]);
     }
 
-    public function showUKT(Request $request){
-        $jalurID = $request->input('jalur');
-        $jalurs = Jalur::all();
-        $jalur = Jalur::find($jalurID);
-        $tgl = Carbon::today();
-        $thnAkademik = ($tgl->month > 6)? $tgl->year.$tgl->year+1 : ($tgl->year-1).$tgl->year;
-        $ukts = UKT::where('TahunAkademik',$thnAkademik)
-            ->where('ProdiID','1')
-            ->where('JalurID', $jalurID)
-            ->get();
-        $data['isShow'] = true;
-        $data['jalurs'] = $jalurs;
-        $data['jalurSelected'] = $jalur;
-        $data['ukts'] = $ukts;
-        return view('ukt.show',$data);
-    }
-
-    public function add($jalurID){
+    /**
+     * @param $jalurID
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function add($prodiId,$jalurID){
         return view('ukt.add');
     }
 
-    public function prosesAdd($jalurID, Request $request){
+    /**
+     * @param $jalurID
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function prosesAdd($prodiId, $jalurId, Request $request){
+        $messages = [
+            'required' => 'Golongan Harus Diisi',
+            'integer' => 'Harus berupa bilangan'
+        ];
+        $validator = Validator::make($request->all(),[
+            'gol.0' => 'required|integer',
+            'gol.1' => 'required|integer',
+            'gol.2' => 'required|integer',
+            'gol.3' => 'required|integer',
+            'gol.4' => 'required|integer',
+            'gol.5' => 'required|integer'
+        ],$messages);
+
+        if($validator->fails()){
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
         $inputs = $request->input('gol');
         $tgl = Carbon::today();
         $thnAkademik = ($tgl->month > 6)? $tgl->year.$tgl->year+1 : ($tgl->year-1).$tgl->year;
-        $prodiID = 1;
         $i = 1;
         foreach ($inputs as $input) {
             $ukt = new UKT();
-            $ukt->ProdiID = $prodiID;
-            $ukt->JalurID = $jalurID;
+            $ukt->ProdiID = $prodiId;
+            $ukt->JalurID = $jalurId;
             $ukt->GolID = $i++;
             $ukt->TahunAkademik = $thnAkademik;
             $ukt->Nominal = $input;
             $ukt->save();
         }
+        return redirect('ukt/'.$prodiId)
+            ->with('pesan','Berhasil ditambahkan');
+    }
 
-        return redirect('ukt');
+    public function detailUKT($prodiId){
+        $prodi = Prodi::find($prodiId);
+        $tgl = Carbon::today();
+        $thnAkademik = ($tgl->month > 6)? $tgl->year.$tgl->year+1 : ($tgl->year-1).$tgl->year;
+        $snmptn = UKT::where('ProdiID',$prodiId)
+            ->where('JalurID',1)
+            ->where('TahunAkademik',$thnAkademik)
+            ->get();
+        $sbmptn = UKT::where('ProdiID',$prodiId)
+            ->where('JalurID',2)
+            ->where('TahunAkademik',$thnAkademik)
+            ->get();
+        $um = UKT::where('ProdiID',$prodiId)
+            ->where('JalurID',3)
+            ->where('TahunAkademik',$thnAkademik)
+            ->get();
+        return view('ukt.detail',[
+            'prodi' => $prodi,
+            'snmptn' => $snmptn,
+            'sbmptn' => $sbmptn,
+            'um' => $um
+        ]);
+    }
+
+    /**
+     * menampilkan total pendapatan dari dana UKT per fakultas
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function danaUKT(){
+        $dana = DanaUKT::getDana();
+        //return $dana;
+        return view('ukt.danaukt',[
+            'dana' => $dana
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * digunakan untuk ajax edit nominal UKT
+     * @return array|string
+     */
+    public function editUKT(Request $request){
+        $validator = Validator::make($request->all(),[
+            'nominal' => 'required|integer',
+        ]);
+
+        if($validator->fails()) return "gagal";
+
+        $uktId = $request->input('uktId');
+        $nominal = $request->input('nominal');
+        $nominal = $request->input('nominal');
+        $ukt = UKT::find($uktId);
+        if($ukt->Nominal != $nominal){
+            $ukt->Nominal = $nominal;
+            $ukt->update();
+        }
+        return $nominal;
+    }
+
+    public function validasi($jalurMasuk){
+        if (! $this->authorize('validasi.ukt')) {
+            return "Not Authorize";
+        }
+        $fakultas = Fakultas::all();
+        $user = Auth::user();
+        $jalur = Jalur::all();
+        return view('ukt.validasi',[
+            'fakultas' => $fakultas,
+            'user' => $user,
+            'jalurPilihan' => $jalurMasuk,
+            'jalurs' => $jalur
+        ]);
+    }
+
+    public function prosesValidasi($prodiID,$jalurMasuk){
+        DanaUKT::validasi($prodiID,$jalurMasuk);
+        return redirect('ukt/validasi/'.$jalurMasuk);
+    }
+
+    public function pdf(){
+        $pdf = PDF::loadView('pdf.invoice', "test");
+        return $pdf->download('invoice.pdf');
     }
 }
